dec18f3a04e77779e5cbb0f96e56671e158be14d
Siddharth Seth <sseth@apache.org>
2015-02-12 11:25:45 -0800

    TEZ-2006. Task communication plane needs to be pluggable. (sseth)

--- tez-dag/src/main/java/org/apache/tez/dag/app/DAGAppMaster.java
+++ tez-dag/src/main/java/org/apache/tez/dag/app/DAGAppMaster.java
@@ -1475,6 +1475,11 @@ public class DAGAppMaster extends AbstractService {
     }
 
     @Override
+    public Credentials getAppCredentials() {
+      return amCredentials;
+    }
+
+    @Override
     public Map<ApplicationAccessType, String> getApplicationACLs() {
       if (getServiceState() != STATE.STARTED) {
         throw new TezUncheckedException(
